a0b6b4187f35c3d3f8310f81ab5e0962e874c768
ArifSanaullah <ak7896023@gmail.com>
2019-10-30 14:34:24 +0500

    with expect's code

--- unwrap/src/main.rs
+++ unwrap/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_variables)]
 use std::fs::File;
 fn main() {
-    let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").unwrap();
+    let g = File::open("hello.txt").expect("file not found.");
 }
