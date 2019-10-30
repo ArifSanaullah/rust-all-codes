7c9819e849810077a9fb905155e2df64c6596c20
ArifSanaullah <ak7896023@gmail.com>
2019-10-30 13:45:43 +0500

    file openin and panicking

--- panicking/src/main.rs
+++ panicking/src/main.rs
@@ -1,4 +1,18 @@
+#![allow(unused_variables)]
+use std::fs::File;
+  //create //module //struct 
 fn main() {
     let v = vec![1, 2, 36];
-    v[10];
+    // v[10];
+
+    
+    // Recoverable erros with Results
+    let f = File::open("hello.txt");
+    //            associated function
+    let f = match f {
+        Ok(file) => file,
+        Err(err) => {
+            panic!("File not found {:#?}",err);
+        },
+    };
 }
